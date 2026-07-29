92ee5155605ed9c1bce4fa453baa2fade787b613
Gabriel Gama <gabrielgamaalves1@gmail.com>
2026-07-29 15:15:18 -0300

    chore: add test implementation code

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
 
 import { getFiberFromElement } from "@lib/react-fiber.js"
 
-import { Carousel, CarouselItem } from "@components/Carousel.js"
+import { Carousel } from "@components/Carousel.js"
+import { CarouselItem } from "@components/CarouselItem.js"
 import { NavigationCarousel } from "./navigation/carousel.js";
 import toUndefinedIfNaN from "@utils/toUndefinedIfNaN.js";
 
 Object.assign(window, {
   getFiberFromElement,
   toUndefinedIfNaN,
-  
+
   __Carousel: Carousel,
   __CarouselItem: CarouselItem,
   __NavigationCarousel: NavigationCarousel
 })
 
 Object.assign(window, {
-  carousel: new Carousel(5),
-  nav_carousel: new NavigationCarousel(5)
+  ca: new Carousel(3),
+  nc: new NavigationCarousel(1)
 })
+
+// [data-card-selected="true"] {
+  // border: 1.5px solid;
+  // border-radius: 10px;
+  // padding: 6px;
+// }
